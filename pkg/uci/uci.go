72beaff4b57efd98ac7584e1f2fd6ebdc750dc15
colmak <69098858+colmak@users.noreply.github.com>
2024-08-28 22:53:28 -0400

    init final content

---
+++ pkg/uci/uci.go
@@ -0,0 +1,10 @@
+// pkg/uci/uci.go
+package uci
+
+import "fmt"
+
+// Start initializes UCI communication.
+func Start() {
+    fmt.Println("UCI protocol started")
+    // Implement UCI command handling here
+}
